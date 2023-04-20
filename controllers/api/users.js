7b6dedd67ef37bf6a327a422114f064f32029c17
Michael Schultz <mikewschultz9029@gmail.com>
2023-04-20 11:42:47 -0400

    added JWT handling demo with comments

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -2,6 +2,18 @@ function create(req, res)
 {
     console.log('[From POST handler')
     console.log(req.body)
+
+    const jwt = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ'
+    // only interested in the payload (claims)
+    const payload = jwt.split('.')[1]
+    // * Node atob deprecated - use Buffer.from(data, 'base64')
+    // ? use below string conversion methods to get appropriate data
+    // * Node btoa deprecated - use buf.toString('base64')
+    // console.log(atob(payload))
+    const buf = Buffer.from(payload, 'base64')
+    console.log(buf.toString('base64'))  // eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9
+    console.log(buf.toString())  // {"sub":"1234567890","name":"John Doe","admin":true}
+
     res.json({
         user:
         {
