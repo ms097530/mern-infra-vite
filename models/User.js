104632dbeb2f3884e0af94210f6f7f973185a8bb
Michael Schultz <mikewschultz9029@gmail.com>
2023-04-20 11:55:48 -0400

    added contents to user model

--- models/User.js
+++ models/User.js
@@ -1,26 +1,49 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const bcrypt = require('bcrypt')
 
-
+const SALT_ROUNDS = 6
 
 // unique: Although technically not a validator, unique: true creates a unique index in the database which will trigger an error if violated.
 // trim: This transform causes Mongoose to trim spaces before and after the string before saving.
 // lowercase: This transform causes Mongoose to convert the string to lowercase before saving.
-const userSchema = new Schema({
-    name: { type: String, required: true },
-    email: {
-        type: String,
-        unique: true,
-        trim: true,
-        lowercase: true,
-        required: true
+const userSchema = new Schema(
+    {
+        name: { type: String, required: true },
+        email: {
+            type: String,
+            unique: true,
+            trim: true,
+            lowercase: true,
+            required: true
+        },
+        password: {
+            type: String,
+            trim: true,
+            minLength: 3,
+            required: true,
+        }
     },
-    password: {
-        type: String,
-        trim: true,
-        minLength: 3,
-        required: true
-    }
-});
+    {
+        timestamps: true,
+        toJSON:
+        {
+            transform: function (doc, ret)
+            {
+                delete ret.password
+                return ret
+            }
+        }
+    });
+
+userSchema.pre('save', async function (next)
+{
+    // * 'this' is the user doc
+    if (!this.isModified('password')) return next()
+
+    // * update the password with the computer hash
+    this.password = await bcrypt.hash(this.password, SALT_ROUNDS)
+    return next()
+})
 
 module.exports = mongoose.model('User', userSchema)
