26b8c00ca8a9eb66cd6da6c81f1131c516faaf72
Michael Schultz <mikewschultz9029@gmail.com>
2023-04-20 11:01:13 -0400

    updated response format

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -2,7 +2,13 @@ function create(req, res)
 {
     console.log('[From POST handler')
     console.log(req.body)
-    res.send(req.body)
+    res.json({
+        user:
+        {
+            name: req.body.name,
+            email: req.body.email
+        }
+    })
 }
 
 module.exports = {
